7e5af0944344813489085e43cf8f9fc82b169299
leighwhite96 <leigh_white@live.co.uk>
2018-01-18 23:48:29 +0000

    chat widget incorporated

--- app/src/App.js
+++ app/src/App.js
@@ -2,32 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { chat } from './api';
-
 import openSocket from 'socket.io-client';
+import { ChatFeed, Message } from 'react-chat-ui';
+import { Widget, addResponseMessage } from 'react-chat-widget';
+
 const socket = openSocket('http://localhost:5000');
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      response: '',
-      value: ''
+      response: ''
     }
     socket.on('fromServer', (res) => this.setState({response: res.server}))
   }
-  handleChange = (e) => {
-    this.setState({
-      value: e.target.value
-    })}
 
-
-  handleSubmit = (e) => {
-    console.log(e);
-    e.preventDefault();
-    socket.emit('fromClient', this.state.value)
+  componentDidMount() {
+    addResponseMessage("Welcome to this awesome chat!");
   }
 
+  addResponse = () => addResponseMessage(this.state.response)
+
+  handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    socket.emit('fromClient', newMessage);
+    setTimeout(this.addResponse,2000)
+  }
 
   render() {
     return (
@@ -36,13 +36,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {this.state.response}
-        <p className="App-intro">
-
-            <input type="text" value={this.state.value} onChange={this.handleChange}/>
-            <button onClick={this.handleSubmit}></button>
-
-        </p>
+        <Widget
+          handleNewUserMessage={this.handleNewUserMessage}
+          profileAvatar='http://clipartist.net/Holidays/Halloween/Halloween/robot_girl_ryofu_quimby_halloween-1969px.png'
+        />
       </div>
     );
   }
